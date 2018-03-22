40c23d980c675e97961ad96e5f8ebd8671fd4749
sheeesh <dc90@live.de>
2018-03-22 21:51:30 +0100

    Insert All

---
+++ PiggyVille2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerController : NetworkBehaviour {
+
+	public Rigidbody rigidbody;
+
+	void Update () {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 7.0f;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 up = transform.TransformDirection(Vector3.up);
+            rigidbody.AddForce(up * 5, ForceMode.Impulse);
+            Debug.Log("Jump!");
+        }
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+	}
+}
---
+++ PiggyVille2/Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        if (isLocalPlayer)
+        { 
+            GetComponent<PlayerController>().enabled = true;
+            Camera.main.transform.position = this.transform.position - this.transform.forward * 10 + this.transform.up * 3;
+            Camera.main.transform.LookAt(this.transform.position);
+            Camera.main.transform.parent = this.transform;
+        }
+	}
+	
+}
